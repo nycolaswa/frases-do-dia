0e1c4425ae664421f4634738a6e5eb539c14ee79
Nycolase <nycolaswa@gmail.com>
2024-12-15 17:20:25 -0300

    Initial Commit

---
+++ frases do dia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  frases do dia
+//
+//  Created by Nycolas William de Andrade on 15/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
